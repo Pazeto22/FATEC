a88c5f35a0a8fb75a4dc7e8d51dcd94f0177b808
Pazeto22 <gustavopazeto95@gmail.com>
2019-10-01 03:20:57 -0300

    Icone para Dakmode

--- js/colormode.js
+++ js/colormode.js
@@ -15,11 +15,12 @@
 function dataSave() {
     let css = document.querySelector("link[href='./css/dados.css']").href
     let icone = document.querySelector("#ColorModeIco").src
+    let logoicone = document.getElementById("logoIco").src
     let onclick = document.getElementById("ColorModeIco")
     let navbar = document.getElementById("nav1").className
     let barraMS = document.getElementById("barraMS").style
 
-    localStorage.setItem("valor", css, icone, onclick, navbar, barraMS); // Salva o valor
+    localStorage.setItem("valor", css, icone, logoicone, onclick, navbar, barraMS); // Salva o valor
     dataGet();
 }
 
@@ -28,6 +29,7 @@ function dataGet() {
 
     document.querySelector("link[href='./css/dados.css']").href = valorTemp;
     document.querySelector("#ColorModeIco").src = valorTemp;
+    let logoicone = document.getElementById("logoIco").src = valorTemp;
     document.getElementById("ColorModeIco") = valorTemp;
     document.getElementById("nav1").className = valorTemp;
     document.getElementById("barraMS").style = valorTemp;
@@ -43,6 +45,7 @@ function StartBody() {
 function DarkMode() {
     document.querySelector("link[href='./css/dados.css']").href = "./css/dados-black.css";
     document.querySelector("#ColorModeIco").src = "./images/colormodeico-black.png"
+    document.getElementById("logoIco").src = "./images/home-logo-black.png"
     document.getElementById("ColorModeIco").setAttribute("onClick", "javascript: LightMode();");
     document.getElementById("nav1").className = "navbar fixed-top navbar-expand-lg navbar navbar-dark bg-dark"
     document.getElementById("barraMS").style = "background:  rgb(33, 35, 41);"
@@ -51,6 +54,7 @@ function DarkMode() {
 function LightMode() {
     document.querySelector("link[href='./css/dados-black.css']").href = "./css/dados.css";
     document.querySelector("#ColorModeIco").src = "./images/colormodeico.png"
+    document.getElementById("logoIco").src = "./images/home-logo.png"
     document.getElementById("ColorModeIco").setAttribute("onClick", "javascript: DarkMode();");
     document.getElementById("nav1").className = "navbar fixed-top navbar-expand-lg navbar-light bg-light"
     document.getElementById("barraMS").style = "background: #d3d3d3;"
